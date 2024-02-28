5f46720f559223db6bd751e1a69b7eb4b18460d9
Limpowitch <timrosendahl2@gmail.com>
2024-02-28 14:08:19 +0100

    Fixat start event

--- src/Event/StartEvent.java
+++ src/Event/StartEvent.java
@@ -3,6 +3,8 @@ package Event;
 import General.Event;
 import General.EventQueue;
 import General.State;
+import State.ArrivalTime;
+import State.StoreState;
 
 public class StartEvent extends Event {
 
@@ -15,7 +17,9 @@ public class StartEvent extends Event {
 		super.execute();
 		state.update(this); // Vi uppdaterar state klockan med executeTime
 		//Uppdatera StoreState med relevant information
+		((StoreState)state).setOpenStatus(true);
 		//Lägg till ArrivalEvent i Queue
+		eventQueue.addToQueue(new ArrivalEvent((StoreState)state, ((StoreState)state).returnArrivalTime(), eventQueue));
 		//ArrivalEvent behöver state, arrivalTime, eventQueue
 	}
 
--- src/State/StoreState.java
+++ src/State/StoreState.java
@@ -1,8 +1,48 @@
 package State;
 
-public class StoreState {
+import General.State;
+
+public class StoreState extends State{
+    final ArrivalTime arrivalTime;
+    final PickTime pickTime;
+    final PayTime payTime;
+    private boolean isOpen;
+	private long seed;
+	private double lambda;
+	private double pickLow;
+	private double pickHigh;
+	private double payLow;
+	private double payHigh;
 	
 	//TODO:
 	//Skapa alla updates/getter för alla olika store-variabler
-
+	public StoreState(double lambda, long seed) {
+		super();
+		this.seed = seed; 
+		this.lambda = lambda; 
+        arrivalTime = new ArrivalTime(lambda, seed);
+        pickTime = new PickTime(pickLow, pickHigh, seed);
+        payTime = new PayTime(payLow, payHigh, seed);
+	}
+	
+	public double returnArrivalTime() {
+		return arrivalTime.generateArrivalTime(returnCurrentTime());
+	}
+	
+	public double returnPickTime() {
+		return pickTime.generatePickTime(returnCurrentTime());
+	}
+	
+	public double returnpayTime() {
+		return payTime.generatePayTime(returnCurrentTime());
+	}
+	
+	
+	public void setOpenStatus(boolean open) {
+		if (open) {
+			this.isOpen = true;
+		} else {
+			this.isOpen = false;
+		}
+	}
 }
