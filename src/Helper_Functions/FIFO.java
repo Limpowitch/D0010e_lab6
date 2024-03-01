ed92a40c0a134a8b3d22edf0c6c1e4b93b3af932
Elias Skeppstedt <88685526+EliasSkeppstedt@users.noreply.github.com>
2024-03-01 21:25:23 +0100

    Update FIFO.java

--- src/Helper_Functions/FIFO.java
+++ src/Helper_Functions/FIFO.java
@@ -1,5 +1,34 @@
 package Helper_Functions;
 
+import java.util.ArrayList;
+
 public class FIFO {
 
+	private ArrayList<Object> FIFO;
+	private int maxSize = 0;
+	
+	public FIFO() {
+		FIFO = new ArrayList<>();
+	}
+	
+	public void add(Object item) {
+		FIFO.add(item);
+        if (FIFO.size() > maxSize()) {
+            maxSize++;
+        }
+	}
+	
+	public void removeFirst() {
+		if (!FIFO.isEmpty()) {
+            FIFO.remove(0);
+        }
+	}
+	
+	public int getSize() {
+		return FIFO.size();
+	}
+	
+	public int maxSize() {
+		return maxSize;
+	}
 }
