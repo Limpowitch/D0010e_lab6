73dca4f074c845ebc60b99a3f8f41eb9cc210f0c
Elias Skeppstedt <elias.skeppstedt@gmail.com>
2024-03-02 13:07:50 +0100

    NewCustomer update

--- src/State/NewCustomer.java
+++ src/State/NewCustomer.java
@@ -1,6 +1,17 @@
 package State;
 
-public class CreateCustomer {
-	// Generera customer
+import State.Customer;
 
+public class NewCustomer {
+	private Customer newCustomer;
+	private static int customerID;
+	public NewCustomer() {
+		newCustomer = new Customer();
+		customerID = newCustomer.getCostumerID();
+		newCustomer.updateCostumerID();
+	}
+	
+	public int getCustomerID() {
+		return customerID;
+	}
 }
