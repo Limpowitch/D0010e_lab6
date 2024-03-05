cf69853d3847834b49e3cd641222be36b5362b15
Elias Skeppstedt <88685526+EliasSkeppstedt@users.noreply.github.com>
2024-03-05 12:10:08 +0100

    Update StoreState.java

--- src/State/StoreState.java
+++ src/State/StoreState.java
@@ -5,47 +5,36 @@ import State.PickTime;
 import State.PayTime;
 
 public class StoreState extends State {
-    final ArrivalTime arrivalTime;
-    final PickTime pickTime;
-    final PayTime payTime;
-    private boolean isOpen;
-    private int maxCapacity;
-    private int customersInStore;
-    private int highestCustomerID;
-    private int missedCustomers;
-    private int paidCustomers;
-    private CheckoutQueue checkoutQueue;
-    private int totalCustomersBeenInQueue;
-    private int customersHasCheckedOut;
-    private int maxCheckoutCapacity;
-    private String latestEventName;
-    private int currentCheckoutCapacity;
-    private int latestEventCustomer;
-	private long seed;
-	private double lambda;
-	private double pickLow;
-	private double pickHigh;
+	final ArrivalTime arrivalTime;
+	private String latestEventName;
+	final PickTime pickTime;
+	final PayTime payTime;
+	private boolean isOpen = false;
+	private int maxCapacity;
+	private int customersInStore = 0;
+	private int highestCustomerID = -1;
+	private int missedCustomers = 0;
+	private int paidCustomers = 0;
+	private CheckoutQueue checkoutQueue;
+	private int totalCustomersBeenInQueue = 0;
+	private int maxCheckoutCapacity;
+	private double payLow;
+	private double payHigh;
+	
+	private int currentCheckoutCapacity;
+	private int latestEventCustomer;
 	private double payLow;
 	private double payHigh;
 	
 	//TODO:
 	//Skapa alla updates/getter för alla olika store-variabler
-	public StoreState(double lambda, long seed, int maxCapacity) {
+		public StoreState(long seed, double lambda, int maxCapacity, int pickLow, int pickHigh) {
 		super();
-		this.seed = seed; 
-		this.lambda = lambda; 
-		this.isOpen = false;
 		this.maxCapacity = maxCapacity;
-		this.customersInStore = 0;
-		this.missedCustomers = 0;
-		this.paidCustomers = 0;
-		this.currentCheckoutCapacity = maxCheckoutCapacity;
-		this.totalCustomersBeenInQueue = 0;
-		highestCustomerID = -1;
-        checkoutQueue = new CheckoutQueue();
-        arrivalTime = new ArrivalTime(lambda, seed);
-        pickTime = new PickTime(pickLow, pickHigh, seed);
-        payTime = new PayTime(payLow, payHigh, seed);
+		checkoutQueue = new CheckoutQueue();
+		arrivalTime = new ArrivalTime(lambda, seed);
+		pickTime = new PickTime(pickLow, pickHigh, seed);
+		payTime = new PayTime(payLow, payHigh, seed);
 	}
 	
 	public double getArrivalTime() {
