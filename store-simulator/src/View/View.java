a3ef37b80c6c2c15ed26f8c4a22b5fa46696d721
Elias Skeppstedt <88685526+EliasSkeppstedt@users.noreply.github.com>
2024-03-13 10:11:03 +0100

    Uppdatering från event branch

--- store-simulator/src/View/View.java
+++ store-simulator/src/View/View.java
@@ -49,57 +49,58 @@ public class View extends GeneralView {
      * Skriver ut parametrarna for simuleringsmodellen.
      */
     public void printBeginStore() {
-        String openingMessage =
-                "PARAMETRAR\n"
-                        + "==========\n"
-                        + "Antal kassor, N..........: " + storeState.getMaxCheckoutCapacity() + "\n"
-                        + "Max som ryms, M..........: " + storeState.getMaxCapacity() + "\n"
-                        + "Ankomshastighet, lambda..: " + storeState.getLambda() + "\n"
-                        + "Plocktider, [P_min..Pmax]: " + storeState.getPickLow() + " " + storeState.getPickHigh() + "\n"
-                        + "Betaltider, [K_min..Kmax]: " + storeState.getPayLow() + " " + storeState.getPayHigh() + "\n"
-                        + "Fro, f...................: " + storeState.getSeed() + "\n\n"
-                        + "FORLOPP\n"
-                        + "=======\n"
-                        + "Tid Handelse Kund ? led ledT I  $ :-( koat  koT koar [Kassako..]\n"
-                        + "0.00   Start\n";
-        System.out.print(openingMessage);
-    }
+		String openingMessage = 
+				"PARAMETRAR\n"
+				+ "==========\n"
+				+ "Antal kassor, N..........: " + storeState.getMaxCheckoutCapacity() + "\n"
+				+ "Max som ryms, M..........: " + storeState.getMaxCapacity() + "\n"
+				+ "Ankomshastighet, lambda..: " + storeState.getLambda() + "\n"
+				+ "Plocktider, [P_min..Pmax]: " + storeState.getPickLow() + " " +  storeState.getPickHigh() + "\n"
+				+ "Betaltider, [K_min..Kmax]: " + storeState.getPayLow() + " " + storeState.getPayHigh() + "\n"
+				+ "Frö, f...................: " + storeState.getSeed() + "\n\n"
+				+ "FÖRLOPP\n"
+				+ "=======\n"
+				+ "Tid Händelse Kund ? led ledT I  $ :-( köat  köT köar [Kassakö..]\n"
+				+ "0.00   Start\n";
+		System.out.print(openingMessage);
+	}
 
     /**
      * Skriver ut uppdaterad information om simuleringstillstandet.
      */
     public void printStoreStats() {
-        String updatingMessage =
-                String.format("%.2f", storeState.returnCurrentTime()) + " "
-                        + String.format("%7s", storeState.getLatestEvent()) + " "
-                        + String.format("%4d", storeState.getLatestEventCustomer()) + " "
-                        + storeState.getOpenStatus() + " "
-                        + String.format("%3d", (storeState.getMaxCheckoutCapacity() - storeState.getCurrentInCheckout())) + " "
-                        + String.format("%.2f", storeState.getEmptyQueueTime()) + " " // Assuming there's a method named getLedigTid in StoreState
-                        + storeState.getCustomersInStore() + " "
-                        + String.format("%2d", storeState.getPaidCustomers()) + " "
-                        + String.format("%3d", storeState.getMissedCustomers()) + " "
-                        + String.format("%4d", storeState.getTotalCustomersBeenInQueue()) + " "
-                        + String.format("%.2f", storeState.getPopulatedQueueTime()) + " " // Assuming there's a method named getKotid in StoreState
-                        + String.format("%4d", storeState.getCheckoutQueue().getSize()) + " "
-                        + storeState.getCheckoutQueue().getCurrentQueue() + "\n\n"; // Assuming there's a method named getKasseStringFormat in StoreState
-        System.out.print(updatingMessage);
-    }
+	    String updatingMessage =
+	    		String.format("%.2f", storeState.returnCurrentTime()) + " "
+	    		+ String.format("%7s", storeState.getLatestEvent()) + " "
+	            + String.format("%4d", storeState.getLatestEventCustomer()) + " "
+	            + storeState.getOpenStatus() + " "
+	            + String.format("%3d", (storeState.getMaxCheckoutCapacity() - storeState.getCurrentInCheckout())) + " "
+	            + String.format("%.2f", storeState.getEmptyQueueTime()) + " " // Assuming there's a method named getLedigTid in StoreState
+	            + storeState.getCustomersInStore() + " "
+	            + String.format("%2d", storeState.getPaidCustomers()) + " "
+	            + String.format("%3d", storeState.getMissedCustomers()) + " "
+	            + String.format("%4d", storeState.getTotalCustomersBeenInQueue()) + " "
+	            + String.format("%.2f", storeState.getPopulatedQueueTime()) + " " // Assuming there's a method named getKötid in StoreState
+	            + String.format("%4d", storeState.getCheckoutQueue().getSize()) + " "
+	            + storeState.getCheckoutQueue().getCurrentQueue() + "\n\n"; // Assuming there's a method named getKasseStringFormat in StoreState
+	    System.out.print(updatingMessage);
+	}
 
     /**
      * Skriver ut slutresultatet av simuleringsmodellen.
      */
     public void printCloseStore() {
-        String closingMessage =
-                "999      End\n"
-                        + "RESULTAT\n"
-                        + "========\n"
-                        + "1) Av " + storeState.getTotalCustomers() + " kunder handlade " + storeState.getPaidCustomers() + " medan " + storeState.getMissedCustomers() + " missades.\n"
-                        + "2) Total tid " + (storeState.getMaxCheckoutCapacity() - storeState.getCurrentInCheckout()) + " kassor varit lediga: " + String.format("%.2f", storeState.getEmptyQueueTime()) + " te. "
-                        + "Genomsnittlig ledig kassatid: " + String.format("%.4s", storeState.getEmptyQueueTime() / storeState.getMaxCheckoutCapacity()) + " te\n"
-                        + "3) Total tid " + storeState.getTotalCustomersBeenInQueue() + " kunder tvingats koa: " + String.format("%.2f", storeState.getPopulatedQueueTime()) + " te.\n"
-                        + "Genomsnittlig kotid: " + String.format("%.4s", storeState.getPopulatedQueueTime() / storeState.getTotalCustomersBeenInQueue()) + " te.\n";
-        System.out.print(closingMessage);
-    }
+	    String closingMessage =
+	    		"999      End\n"
+	            + "RESULTAT\n"
+	            + "========\n"
+	            + "1) Av " + storeState.getTotalCustomers() + " kunder handlade " + storeState.getPaidCustomers() + " medan " + storeState.getMissedCustomers() + " missades.\n"
+	            + "2) Total tid " + (storeState.getMaxCheckoutCapacity() - storeState.getCurrentInCheckout()) + " kassor varit lediga: " + String.format("%.2f", storeState.getEmptyQueueTime()) + " te. \n"
+	            + "Genomsnittlig ledig kassatid: " + String.format("%.5s", storeState.getEmptyQueueTime()/storeState.getMaxCheckoutCapacity()) + " te "
+        		+ "(dvs. " + String.format("%.5s",((storeState.getEmptyQueueTime()/storeState.getMaxCheckoutCapacity()) / storeState.returnRealTime()) * 100) + "% " + "av tiden från öppning tills sista kunden\r\n"
+	            + "3) Total tid " + storeState.getTotalCustomersBeenInQueue() + " kunder tvingats köa: " + String.format("%.2f", storeState.getPopulatedQueueTime()) + " te.\n"
+	            + "Genomsnittlig kötid: " + String.format("%.5s", storeState.getPopulatedQueueTime()/storeState.getTotalCustomersBeenInQueue())+ " te.\n";
+	    System.out.print(closingMessage);
+	}
 
 }
