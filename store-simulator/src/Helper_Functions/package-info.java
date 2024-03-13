1b672471645ad5da8ea2673ab593a15f4be3c701
Elias Skeppstedt <88685526+EliasSkeppstedt@users.noreply.github.com>
2024-03-13 11:03:35 +0100

    Tillagd package-info.java

---
+++ store-simulator/src/Helper_Functions/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Paketet Helper_Functions innnehaller klasser vars metoder och variabler anvands som hjalp- och styrmedel till simulationen.
+ *
+ * @author Bernhard Rosenzweig, Elias Skeppstedt, Tim Rosendahl, Axel Sandstrom.
+ */
+package Helper_Functions;
