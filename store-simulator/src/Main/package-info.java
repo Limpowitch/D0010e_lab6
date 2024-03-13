5cb5952098c3b6aecbf292b4d4cc65d5b7ce7c71
Elias Skeppstedt <88685526+EliasSkeppstedt@users.noreply.github.com>
2024-03-13 11:04:15 +0100

    Tillagd package-info.java

---
+++ store-simulator/src/Main/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Paketet Main innehåller startprogrammet för simulationen samt simulatorns optimeringsprogram.
+ *
+ * @author Bernhard Rosenzweig, Elias Skeppstedt, Tim Rosendahl, Axel Sandstrom.
+ */
+package Main;
