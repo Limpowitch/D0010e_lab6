73996f04e46061ee50fb326db40299ed9327c868
Elias Skeppstedt <88685526+EliasSkeppstedt@users.noreply.github.com>
2024-03-13 11:05:20 +0100

    Tillagd package-info.java

---
+++ store-simulator/src/View/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Paketet View innehaller simulatorns vy, i form av utprintningar i terminalen.
+ *
+ * @author Bernhard Rosenzweig, Elias Skeppstedt, Tim Rosendahl, Axel Sandstrom.
+ */
+package View;
