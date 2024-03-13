6c3d6668bd6aca7970564578651b20da72e82bac
Elias Skeppstedt <88685526+EliasSkeppstedt@users.noreply.github.com>
2024-03-13 11:04:46 +0100

    Tillagd package-info.java

---
+++ store-simulator/src/State/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Paketet State innehåller information om simulationens nuvarande lage.
+ *
+ * @author Bernhard Rosenzweig, Elias Skeppstedt, Tim Rosendahl, Axel Sandstrom.
+ */
+package State;
