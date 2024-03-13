46d2b8ca1b6e1a48e9af27ea764b795fa28a4356
Elias Skeppstedt <88685526+EliasSkeppstedt@users.noreply.github.com>
2024-03-13 11:02:41 +0100

    Tillagd package-info.java

---
+++ store-simulator/src/General/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Paketet General innnehaller alla generiska eventklasse, som ska kunna anvandas till en godtycklig simulering.
+ *
+ * @author Bernhard Rosenzweig, Elias Skeppstedt, Tim Rosendahl, Axel Sandstrom.
+ */
+package General;
